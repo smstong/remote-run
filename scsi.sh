f6d9099b1c24b79de34c198bdf95a9dd13c10e04
Jonathan <jonathan.jq.zhao@gmail.com>
2018-05-09 14:38:34 -0400

    handle both el5 and el7

--- scsi.sh
+++ scsi.sh
@@ -2,6 +2,8 @@
 
 printf "%-8s%-10s%-20s%-10s\n" "DISK" "VENDOR" "host:chanel:id:lun" "SIZE(GB)"
 
+OS=$(uname -r)
+
 for sd in /sys/block/sd*
 do
 	DISK=$(basename $sd)
@@ -9,7 +11,11 @@ do
 	SIZE=$(cat size | awk '{printf("%-.2f", $1/(1024.0*1024.0))}')
 	cd $sd/device
 	VENDOR=$(cat vendor)
-	SCSI=$(ls -d scsi_device:* | sed 's/scsi_device://')
+	if echo $OS | grep el5 >/dev/null; then
+		SCSI=$(ls -d scsi_device:* | sed 's/scsi_device://')
+	else
+		SCSI=$(ls scsi_device)
+	fi
 
 	printf "%-8s%-15s%-20s%-10.2f\n" $DISK $VENDOR $SCSI $SIZE
 
